b1be8904a813913dede18bdabb5d0fb231b99be7
AitorFagoaga <aitor.fagoaga2001@gmail.com>
2024-11-27 22:49:44 -0300

    creation of brand and categories started

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -3,22 +3,16 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
-import { FormBrands } from "/Users/aitorfagoaga/Documents/projects/project/components/create/form-brands"
+import { AllForms } from "./components/AllForms"
 import { CreateBrandDialog } from "@/components/brands/create-brand-dialog";
+import { FormBrands } from "./components/FormBrands";
 
 export default function CreatePage() {
   const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
 
   return (
     <div className="space-y-4">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Brands</h1>
-        <Button onClick={() => setIsCreateDialogOpen(true)}>
-          <PlusIcon className="h-4 w-4 mr-2" />
-          Add Brand
-        </Button>
-      </div>
-      <FormBrands />
+      <AllForms />
       <CreateBrandDialog
         open={isCreateDialogOpen}
         onOpenChange={setIsCreateDialogOpen}
